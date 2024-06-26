835b0998d7c803674089d90b28e0a7f1386c7d04
gonzalofch <gmfloresch@gmail.com>
2024-06-26 08:56:24 +0200

    SingleResponsability Video Start

---
+++ SingleResponsability/ExampleFromVideo/Person.cs
@@ -0,0 +1,11 @@
+namespace ExampleFromVideo;
+
+public class Person
+{
+    public Person()
+    {
+    }
+
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
---
+++ SingleResponsability/ExampleFromVideo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace ExampleFromVideo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to my application!");
+
+//ask for user information
+        Person user = new Person();
+
+        Console.Write("What is your first name: ");
+        user.FirstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        user.LastName = Console.ReadLine();
+
+//Checks to be sure the first and last names are valid
+        if (string.IsNullOrWhiteSpace(user.FirstName))
+        {
+            Console.WriteLine("You did not give us a valid first name!");
+            Console.ReadLine();
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(user.LastName))
+        {
+            Console.WriteLine("Ypu did not give us a valid last name!");
+            Console.ReadLine();
+            return;
+        }
+
+//Create a username for the person
+        Console.WriteLine(new StringBuilder().Append("Your username is ")
+            .Append(user.FirstName.Substring(0, 1))
+            .Append(user.LastName)
+            .ToString());
+    }
+}
