c3b0cf61c93940433461b9115ecdaf75579ebbe5
gonzalofch <gmfloresch@gmail.com>
2024-06-27 14:04:56 +0200

    startPoint liskov substitution principle

---
+++ LiskovSubstitution/LiskovSubstitution/LSLibrary/Employee.cs
@@ -0,0 +1,20 @@
+﻿namespace LSLibrary;
+
+public class Employee
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Employee Manager { get; set; } = null;
+    public decimal Salary { get; set; }
+
+    public virtual void AssignManager(Employee manager)
+    {
+        Manager = manager;
+    }
+
+    public virtual void CalculatePerHourRate(int rank)
+    {
+        decimal baseAmount = 12.50M;
+        Salary = baseAmount + (rank * 2);
+    }
+}
---
+++ LiskovSubstitution/LiskovSubstitution/LiskovSubstitution/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using LSLibrary;
+
+namespace LiskovSubstitution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Manager accountingVp = new Manager();
+        accountingVp.FirstName = "Emma";
+        accountingVp.LastName = "Stone";
+        accountingVp.CalculatePerHourRate(4);
+
+        Employee emp = new CEO();
+        emp.FirstName = "TIm";
+        emp.LastName = "Corey";
+        emp.AssignManager(accountingVp);
+        emp.CalculatePerHourRate(2);
+    
+        Console.WriteLine(new StringBuilder().Append(emp.FirstName)
+            .Append("'s salary is $")
+            .Append(emp.Salary)
+            .Append("/hour.")
+            .ToString());
+        Console.ReadLine();
+    }
+}
