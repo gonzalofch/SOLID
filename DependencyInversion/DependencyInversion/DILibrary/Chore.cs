931b7d313f5d9267932425f9fdced6426710e972
gonzalofch <gmfloresch@gmail.com>
2024-07-04 11:21:01 +0200

    Deleted dependencies from Chore Class

--- DependencyInversion/DependencyInversion/DILibrary/Chore.cs
+++ DependencyInversion/DependencyInversion/DILibrary/Chore.cs
@@ -2,23 +2,31 @@
 
 public class Chore : IChore
 {
+    //dependencias inyectadas en esta clase
+    private ILogger _logger;
+    private IMessageSender _messageSender;
+
     public string ChoreName { get; set; }
     public IPerson Owner { get; set; }
     public double HoursWorked { get; private set; }
     public bool IsComplete { get; private set; }
 
+    public Chore(ILogger logger, IMessageSender messageSender)
+    {
+        _logger = logger;
+        _messageSender = messageSender;
+    }
+
     public void PerformedWork(double hours)
     {
         HoursWorked += hours;
-        Logger log = new Logger();
-        log.Log($"Performed work on {ChoreName}");
+        _logger.Log($"Performed work on {ChoreName}");
     }
 
     public void CompleteChore()
     {
         IsComplete = true;
-        Logger log = new Logger();
-        Emailer emailer = new Emailer();
-        emailer.SendMessage(Owner, $"The chore {ChoreName} is complete");
+        _logger.Log($"Completed {ChoreName}");
+        _messageSender.SendMessage(Owner, $"The chore {ChoreName} is complete");
     }
 }
